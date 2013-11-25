70f636acefe4ffde4fdcb93783de64f864c0353d
Guoqing Zheng <gzheng@andrew.cmu.edu>
2013-11-25 11:31:24 -0500

    new result should be 0.35

--- qa4mre-alzheimer-task/src/main/java/edu/cmu/lti/deiis/hw5/answer_ranking/AnswerChoiceCandAnsPMIScorer.java
+++ qa4mre-alzheimer-task/src/main/java/edu/cmu/lti/deiis/hw5/answer_ranking/AnswerChoiceCandAnsPMIScorer.java
@@ -27,178 +27,189 @@ import edu.cmu.lti.qalab.utils.Utils;
 
 public class AnswerChoiceCandAnsPMIScorer extends JCasAnnotator_ImplBase {
 
-	private SolrWrapper solrWrapper;
-	HashSet<String> hshStopWords = new HashSet<String>();
-	int K_CANDIDATES = 5;
+  private SolrWrapper solrWrapper;
 
-	@Override
-	public void initialize(UimaContext context) throws ResourceInitializationException {
-		super.initialize(context);
-		String serverUrl = (String) context.getConfigParameterValue("SOLR_SERVER_URL");
-		K_CANDIDATES = (Integer) context.getConfigParameterValue("K_CANDIDATES");
+  HashSet<String> hshStopWords = new HashSet<String>();
 
-		try {
-			this.solrWrapper = new SolrWrapper(serverUrl);
-			// loadStopWords(stopFile);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+  int K_CANDIDATES = 5;
 
-	}
+  @Override
+  public void initialize(UimaContext context) throws ResourceInitializationException {
+    super.initialize(context);
+    String serverUrl = (String) context.getConfigParameterValue("SOLR_SERVER_URL");
+    K_CANDIDATES = (Integer) context.getConfigParameterValue("K_CANDIDATES");
 
-	@Override
-	public void process(JCas aJCas) throws AnalysisEngineProcessException {
-		System.out.println("Running in PMI");
+    try {
+      this.solrWrapper = new SolrWrapper(serverUrl);
+      // loadStopWords(stopFile);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
 
-		TestDocument testDoc = Utils.getTestDocumentFromCAS(aJCas);
-		// String testDocId = testDoc.getId();
-		ArrayList<QuestionAnswerSet> qaSet = Utils.getQuestionAnswerSetFromTestDocCAS(aJCas);
+  }
 
-		for (int i = 0; i < qaSet.size(); i++) {
+  @Override
+  public void process(JCas aJCas) throws AnalysisEngineProcessException {
+    System.out.println("Running in PMI");
 
-			Question question = qaSet.get(i).getQuestion();
-			System.out.println("Question: " + question.getText());
-			ArrayList<Answer> choiceList = Utils.fromFSListToCollection(qaSet.get(i).getAnswerList(), Answer.class);
-			ArrayList<CandidateSentence> candSentList = Utils
-					.fromFSListToCollection(qaSet.get(i).getCandidateSentenceList(), CandidateSentence.class);
+    TestDocument testDoc = Utils.getTestDocumentFromCAS(aJCas);
+    // String testDocId = testDoc.getId();
+    ArrayList<QuestionAnswerSet> qaSet = Utils.getQuestionAnswerSetFromTestDocCAS(aJCas);
 
-			int topK = Math.min(K_CANDIDATES, candSentList.size());
-			for (int c = 0; c < topK; c++) {
+    for (int i = 0; i < qaSet.size(); i++) {
 
-				CandidateSentence candSent = candSentList.get(c);
+      Question question = qaSet.get(i).getQuestion();
+      System.out.println("Question: " + question.getText());
+      ArrayList<Answer> choiceList = Utils.fromFSListToCollection(qaSet.get(i).getAnswerList(),
+              Answer.class);
+      ArrayList<CandidateSentence> candSentList = Utils.fromFSListToCollection(qaSet.get(i)
+              .getCandidateSentenceList(), CandidateSentence.class);
 
-				ArrayList<NounPhrase> candSentNouns = Utils.fromFSListToCollection(candSent.getSentence().getPhraseList(), NounPhrase.class);
-				ArrayList<NER> candSentNers = Utils.fromFSListToCollection(candSent.getSentence().getNerList(), NER.class);
+      int topK = Math.min(K_CANDIDATES, candSentList.size());
+      for (int c = 0; c < topK; c++) {
 
-				ArrayList<CandidateAnswer> candAnsList = new ArrayList<CandidateAnswer>();
-				for (int j = 0; j < choiceList.size(); j++) {
-					double score1 = 0.0;
-					Answer answer = choiceList.get(j);
+        CandidateSentence candSent = candSentList.get(c);
 
-					for (int k = 0; k < candSentNouns.size(); k++) {
-						try {
-							score1 += scoreCoOccurInSameDoc(candSentNouns.get(k).getText(), choiceList.get(j));
-						} catch (Exception e) {
-							e.printStackTrace();
-						}
-					}
+        ArrayList<NounPhrase> candSentNouns = Utils.fromFSListToCollection(candSent.getSentence()
+                .getPhraseList(), NounPhrase.class);
+        ArrayList<NER> candSentNers = Utils.fromFSListToCollection(candSent.getSentence()
+                .getNerList(), NER.class);
 
-					for (int k = 0; k < candSentNers.size(); k++) {
+        ArrayList<CandidateAnswer> candAnsList = new ArrayList<CandidateAnswer>();
+        for (int j = 0; j < choiceList.size(); j++) {
+          double score1 = 0.0;
+          Answer answer = choiceList.get(j);
 
-						try {
-							score1 += scoreCoOccurInSameDoc(candSentNers.get(k).getText(), choiceList.get(j));
-						} catch (Exception e) {
-							e.printStackTrace();
-						}
+          for (int k = 0; k < candSentNouns.size(); k++) {
+            try {
+              score1 += scoreCoOccurInSameDoc(candSentNouns.get(k).getText(), choiceList.get(j));
+            } catch (Exception e) {
+              e.printStackTrace();
+            }
+          }
 
-					}
+          for (int k = 0; k < candSentNers.size(); k++) {
 
-					System.out.println(choiceList.get(j).getText() + "\t" + score1 + "\t" + ((score1)));
+            try {
+              score1 += scoreCoOccurInSameDoc(candSentNers.get(k).getText(), choiceList.get(j));
+            } catch (Exception e) {
+              e.printStackTrace();
+            }
 
-					CandidateAnswer candAnswer = null;
-					if (candSent.getCandAnswerList() == null) {
-						candAnswer = new CandidateAnswer(aJCas);
-					} else {
-						candAnswer = Utils.fromFSListToCollection(candSent.getCandAnswerList(), CandidateAnswer.class).get(j);// new
-																																// CandidateAnswer(aJCas);;
-					}
-					candAnswer.setText(answer.getText());
-					candAnswer.setQId(answer.getQuestionId());
-					candAnswer.setChoiceIndex(j);
-					candAnswer.setPMIScore(score1);
-					candAnsList.add(candAnswer);
-				}
-				FSList fsCandAnsList = Utils.fromCollectionToFSList(aJCas, candAnsList);
-				candSent.setCandAnswerList(fsCandAnsList);
-				candSentList.set(c, candSent);
-			}
+          }
+          
+          
 
-			System.out.println("================================================");
-			FSList fsCandSentList = Utils.fromCollectionToFSList(aJCas, candSentList);
-			qaSet.get(i).setCandidateSentenceList(fsCandSentList);
+          System.out.println(choiceList.get(j).getText() + "\t" + score1 + "\t" + ((score1)));
 
-		}
-		FSList fsQASet = Utils.fromCollectionToFSList(aJCas, qaSet);
-		testDoc.setQaList(fsQASet);
+          CandidateAnswer candAnswer = null;
+          if (candSent.getCandAnswerList() == null) {
+            candAnswer = new CandidateAnswer(aJCas);
+          } else {
+            candAnswer = Utils.fromFSListToCollection(candSent.getCandAnswerList(),
+                    CandidateAnswer.class).get(j);// new
+            // CandidateAnswer(aJCas);;
+          }
+          candAnswer.setText(answer.getText());
+          candAnswer.setQId(answer.getQuestionId());
+          candAnswer.setChoiceIndex(j);
+          candAnswer.setPMIScore(score1);
+          candAnsList.add(candAnswer);
+        }
+        FSList fsCandAnsList = Utils.fromCollectionToFSList(aJCas, candAnsList);
+        candSent.setCandAnswerList(fsCandAnsList);
+        candSentList.set(c, candSent);
+      }
 
-	}
+      System.out.println("================================================");
+      FSList fsCandSentList = Utils.fromCollectionToFSList(aJCas, candSentList);
+      qaSet.get(i).setCandidateSentenceList(fsCandSentList);
 
-	public double scoreCoOccurInSameDoc(String question, Answer choice) throws Exception {
-		// String choiceTokens[] = choice.split("[ ]");
-		ArrayList<NounPhrase> choiceNounPhrases = Utils.fromFSListToCollection(choice.getNounPhraseList(), NounPhrase.class);
-		double score = 0.0;
+    }
+    FSList fsQASet = Utils.fromCollectionToFSList(aJCas, qaSet);
+    testDoc.setQaList(fsQASet);
 
-		for (int i = 0; i < choiceNounPhrases.size(); i++) {
-			// score1(choicei) = hits(problem AND choicei) / hits(choicei)
-			String choiceNounPhrase = choiceNounPhrases.get(i).getText();
-			if (question.split("[ ]").length > 1) {
-				question = "\"" + question + "\"";
-			}
-			if (choiceNounPhrase.split("[ ]").length > 1) {
-				choiceNounPhrase = "\"" + choiceNounPhrase + "\"";
-			}
+  }
 
-			String query = question + " AND " + choiceNounPhrase;
-			// System.out.println(query);
-			HashMap<String, String> params = new HashMap<String, String>();
-			params.put("q", query);
-			params.put("rows", "1");
-			SolrParams solrParams = new MapSolrParams(params);
-			QueryResponse rsp = null;
-			long combinedHits = 0;
-			try {
-				rsp = solrWrapper.getServer().query(solrParams);
-				combinedHits = rsp.getResults().getNumFound();
-			} catch (Exception e) {
-				e.printStackTrace();
-//				System.out.println(e + "\t" + query);
-			}
+  public double scoreCoOccurInSameDoc(String question, Answer choice) throws Exception {
+    // String choiceTokens[] = choice.split("[ ]");
+    ArrayList<NounPhrase> choiceNounPhrases = Utils.fromFSListToCollection(
+            choice.getNounPhraseList(), NounPhrase.class);
+    double score = 0.0;
 
-			// System.out.println(query+"\t"+combinedHits);
+    for (int i = 0; i < choiceNounPhrases.size(); i++) {
+      // score1(choicei) = hits(problem AND choicei) / hits(choicei)
+      String choiceNounPhrase = choiceNounPhrases.get(i).getText();
+      if (question.split("[ ]").length > 1) {
+        question = "\"" + question + "\"";
+      }
+      if (choiceNounPhrase.split("[ ]").length > 1) {
+        choiceNounPhrase = "\"" + choiceNounPhrase + "\"";
+      }
 
-			query = choiceNounPhrase;
-			// System.out.println(query);
-			params = new HashMap<String, String>();
-			params.put("q", query);
-			params.put("rows", "1");
-			solrParams = new MapSolrParams(params);
+      String query = question + " AND " + choiceNounPhrase;
+      // System.out.println(query);
+      HashMap<String, String> params = new HashMap<String, String>();
+      params.put("q", query);
+      params.put("rows", "1");
+      SolrParams solrParams = new MapSolrParams(params);
+      QueryResponse rsp = null;
+      long combinedHits = 0;
+      try {
+        rsp = solrWrapper.getServer().query(solrParams);
+        combinedHits = rsp.getResults().getNumFound();
+      } catch (Exception e) {
+        e.printStackTrace();
+        // System.out.println(e + "\t" + query);
+      }
 
-			long nHits1 = 0;
-			try {
-				rsp = solrWrapper.getServer().query(solrParams);
-				nHits1 = rsp.getResults().getNumFound();
-			} catch (Exception e) {
-				e.printStackTrace();
-//				System.out.println(e + "\t" + query);
-			}
-			// System.out.println("Query hits " + query + "\t" + nHits1);
+      // System.out.println(query+"\t"+combinedHits);
 
-			/*
-			 * query = question; // System.out.println(query); params = new
-			 * HashMap<String, String>(); params.put("q", query);
-			 * params.put("rows", "1"); solrParams = new MapSolrParams(params);
-			 * rsp = solrWrapper.getServer().query(solrParams); long nHits2 =
-			 * rsp.getResults().getNumFound(); //
-			 * System.out.println(query+"\t"+nHits2);
-			 */
+      query = choiceNounPhrase;
+      // System.out.println(query);
+      params = new HashMap<String, String>();
+      params.put("q", query);
+      params.put("rows", "1");
+      solrParams = new MapSolrParams(params);
 
-			// score += myLog(combinedHits, nHits1, nHits2);
-			if (nHits1 != 0) {
-				score += (double) combinedHits / nHits1;
-			}
-		}
-		if (choiceNounPhrases.size() > 0) {
-			// score=score/choiceNounPhrases.size();
-		}
-		return score;
-	}
+      long nHits1 = 0;
+      try {
+        rsp = solrWrapper.getServer().query(solrParams);
+        nHits1 = rsp.getResults().getNumFound();
+      } catch (Exception e) {
+        e.printStackTrace();
+        // System.out.println(e + "\t" + query);
+      }
+      // System.out.println("Query hits " + query + "\t" + nHits1);
 
-	public double myLog(long combined, long nHits1, long nHits2) {
-		if (combined == 0 || nHits1 == 0 || nHits2 == 0) {
-			return 0;
-		}
-		double logValue = Math.log(combined) - Math.log(nHits1) - Math.log(nHits2);
-		return logValue;
-	}
+//      query = question; 
+//      // System.out.println(query);
+//      params = new HashMap<String, String>();
+//      params.put("q", query);
+//      params.put("rows", "1");
+//      solrParams = new MapSolrParams(params);
+//      rsp = solrWrapper.getServer().query(solrParams);
+//      long nHits2 = rsp.getResults().getNumFound(); //
+//      System.out.println(query + "\t" + nHits2);
+//
+//      score += myLog(combinedHits, nHits1, nHits2);
+
+       if (nHits1 != 0) {
+       score += (double) combinedHits / nHits1;
+       }
+    }
+    if (choiceNounPhrases.size() > 0) {
+       //score=score/choiceNounPhrases.size();
+    }
+    return score;
+  }
+
+  public double myLog(long combined, long nHits1, long nHits2) {
+    if (combined == 0 || nHits1 == 0 || nHits2 == 0) {
+      return 0;
+    }
+    double logValue = Math.log(combined) - Math.log(nHits1) - Math.log(nHits2);
+    return logValue;
+  }
 
 }
--- qa4mre-alzheimer-task/src/main/java/edu/cmu/lti/deiis/hw5/answer_selection/AnswerSelectionByKCandVoting.java
+++ qa4mre-alzheimer-task/src/main/java/edu/cmu/lti/deiis/hw5/answer_selection/AnswerSelectionByKCandVoting.java
@@ -21,146 +21,180 @@ import edu.cmu.lti.qalab.utils.Utils;
 
 public class AnswerSelectionByKCandVoting extends JCasAnnotator_ImplBase {
 
-	int K_CANDIDATES = 5;
+  int K_CANDIDATES = 5;
 
-	List<Double> cAt1ForAll = new ArrayList<Double>();
-	List<Integer> numCorrectForAll = new ArrayList<Integer>();
-	List<Integer> numTotalAnswerForAll = new ArrayList<Integer>();
+  List<Double> cAt1ForAll = new ArrayList<Double>();
 
-	@Override
-	public void initialize(UimaContext context) throws ResourceInitializationException {
-		super.initialize(context);
-		K_CANDIDATES = (Integer) context.getConfigParameterValue("K_CANDIDATES");
-	}
+  List<Integer> numCorrectForAll = new ArrayList<Integer>();
 
-	@Override
-	public void process(JCas aJCas) throws AnalysisEngineProcessException {
-		TestDocument testDoc = Utils.getTestDocumentFromCAS(aJCas);
-		ArrayList<QuestionAnswerSet> qaSet = Utils.fromFSListToCollection(testDoc.getQaList(), QuestionAnswerSet.class);
-		int matched = 0;
-		int total = 0;
-		int unanswered = 0;
+  List<Integer> numTotalAnswerForAll = new ArrayList<Integer>();
 
-		for (int i = 0; i < qaSet.size(); i++) {
+  @Override
+  public void initialize(UimaContext context) throws ResourceInitializationException {
+    super.initialize(context);
+    K_CANDIDATES = (Integer) context.getConfigParameterValue("K_CANDIDATES");
+  }
 
-			Question question = qaSet.get(i).getQuestion();
-			System.out.println("Question: " + question.getText());
-			ArrayList<Answer> choiceList = Utils.fromFSListToCollection(qaSet.get(i).getAnswerList(), Answer.class);
-			ArrayList<CandidateSentence> candSentList = Utils
-					.fromFSListToCollection(qaSet.get(i).getCandidateSentenceList(), CandidateSentence.class);
+  @Override
+  public void process(JCas aJCas) throws AnalysisEngineProcessException {
+    TestDocument testDoc = Utils.getTestDocumentFromCAS(aJCas);
+    ArrayList<QuestionAnswerSet> qaSet = Utils.fromFSListToCollection(testDoc.getQaList(),
+            QuestionAnswerSet.class);
+    int matched = 0;
+    int total = 0;
+    int unanswered = 0;
 
-			int topK = Math.min(K_CANDIDATES, candSentList.size());
-			String correct = "";
+    for (int i = 0; i < qaSet.size(); i++) {
 
-			for (int j = 0; j < choiceList.size(); j++) {
-				Answer answer = choiceList.get(j);
-				if (answer.getIsCorrect()) {
-					correct = answer.getText();
-					break;
-				}
-			}
+      Question question = qaSet.get(i).getQuestion();
+      System.out.println("Question: " + question.getText());
+      ArrayList<Answer> choiceList = Utils.fromFSListToCollection(qaSet.get(i).getAnswerList(),
+              Answer.class);
+      ArrayList<CandidateSentence> candSentList = Utils.fromFSListToCollection(qaSet.get(i)
+              .getCandidateSentenceList(), CandidateSentence.class);
 
-			HashMap<String, Double> hshAnswer = new HashMap<String, Double>();
+      int topK = Math.min(K_CANDIDATES, candSentList.size());
+      String correct = "";
 
-			for (int c = 0; c < topK; c++) {
+      for (int j = 0; j < choiceList.size(); j++) {
+        Answer answer = choiceList.get(j);
+        if (answer.getIsCorrect()) {
+          correct = answer.getText();
+          break;
+        }
+      }
 
-				CandidateSentence candSent = candSentList.get(c);
+      HashMap<String, Double> hshAnswer = new HashMap<String, Double>();
 
-				ArrayList<CandidateAnswer> candAnswerList = Utils.fromFSListToCollection(candSent.getCandAnswerList(), CandidateAnswer.class);
-				String selectedAnswer = "";
-				double maxScore = Double.NEGATIVE_INFINITY;
-				for (int j = 0; j < candAnswerList.size(); j++) {
+//      for (int c = 0; c < topK; c++) {
+//
+//        CandidateSentence candSent = candSentList.get(c);
+//
+//        ArrayList<CandidateAnswer> candAnswerList = Utils.fromFSListToCollection(
+//                candSent.getCandAnswerList(), CandidateAnswer.class);
+//        String selectedAnswer = "";
+//        double maxScore = Double.NEGATIVE_INFINITY;
+//        for (int j = 0; j < candAnswerList.size(); j++) {
+//
+//          CandidateAnswer candAns = candAnswerList.get(j);
+//          String answer = candAns.getText();
+//
+//          double totalScore = candAns.getSimilarityScore() + candAns.getSynonymScore()
+//                  + candAns.getPMIScore();
+//
+//          if (totalScore > maxScore) {
+//            maxScore = totalScore;
+//            selectedAnswer = answer;
+//          }
+//        }
+//        Double existingVal = hshAnswer.get(selectedAnswer);
+//        if (existingVal == null) {
+//          existingVal = new Double(0.0);
+//        }
+//        hshAnswer.put(selectedAnswer, existingVal + 1.0);
+//      }
 
-					CandidateAnswer candAns = candAnswerList.get(j);
-					String answer = candAns.getText();
+      // GUOQING
+      for (int c = 0; c < topK; c++) {
 
-					double totalScore = candAns.getSimilarityScore() + candAns.getSynonymScore() + candAns.getPMIScore();
+        CandidateSentence candSent = candSentList.get(c);
 
-					if (totalScore > maxScore) {
-						maxScore = totalScore;
-						selectedAnswer = answer;
-					}
-				}
-				Double existingVal = hshAnswer.get(selectedAnswer);
-				if (existingVal == null) {
-					existingVal = new Double(0.0);
-				}
-				hshAnswer.put(selectedAnswer, existingVal + 1.0);
-			}
+        ArrayList<CandidateAnswer> candAnswerList = Utils.fromFSListToCollection(
+                candSent.getCandAnswerList(), CandidateAnswer.class);
 
-			String bestChoice = null;
-			try {
-				bestChoice = findBestChoice(hshAnswer);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			System.out.println("Correct Choice: " + "\t" + correct);
-			System.out.println("Best Choice: " + "\t" + bestChoice);
+        for (int j = 0; j < candAnswerList.size(); j++) {
 
-			if (bestChoice == null) {
-				unanswered++;
-			}
-			if (bestChoice != null && correct.equals(bestChoice)) {
-				matched++;
+          CandidateAnswer candAns = candAnswerList.get(j);
+          String answer = candAns.getText();
 
-			}
-			total++;
-			System.out.println("================================================");
+          double totalScore = candAns.getSimilarityScore() + candAns.getSynonymScore()
+                  + candAns.getPMIScore();
 
-		}
+          
+          Double existingVal = hshAnswer.get(answer);
+          if (existingVal == null) {
+            existingVal = new Double(0.0);
+          }
+          hshAnswer.put(answer, existingVal + totalScore);
+        }
+      }
 
-		System.out.println("Correct: " + matched + "/" + total + "=" + ((matched * 100.0) / total) + "%");
-		// TO DO: Reader of this pipe line should read from xmi generated by
-		// SimpleRunCPE
-		double cAt1 = (((double) matched) / ((double) total) * unanswered + (double) matched) * (1.0 / total);
+      String bestChoice = null;
+      try {
+        bestChoice = findBestChoice(hshAnswer);
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+      System.out.println("Correct Choice: " + "\t" + correct);
+      System.out.println("Best Choice: " + "\t" + bestChoice);
 
-		cAt1ForAll.add(cAt1);
-		numCorrectForAll.add(matched);
-		numTotalAnswerForAll.add(total);
+      if (bestChoice == null) {
+        unanswered++;
+      }
+      if (bestChoice != null && correct.equals(bestChoice)) {
+        matched++;
 
-		System.out.println("c@1 score:" + cAt1);
-	}
+      }
+      total++;
+      System.out.println("================================================");
 
-	public String findBestChoice(HashMap<String, Double> hshAnswer) throws Exception {
-		Iterator<String> it = hshAnswer.keySet().iterator();
-		String bestAns = null;
-		double maxScore = 0;
-		System.out.println("Aggregated counts; ");
-		while (it.hasNext()) {
-			String key = it.next();
-			Double val = hshAnswer.get(key);
-			System.out.println(key + "\t" + key + "\t" + val);
-			if (val > maxScore) {
-				maxScore = val;
-				bestAns = key;
-			}
-		}
-		return bestAns;
-	}
+    }
 
-	@Override
-	public void collectionProcessComplete() throws AnalysisEngineProcessException {
-		System.out.println("======================Performance in text======================");
+    System.out.println("Correct: " + matched + "/" + total + "=" + ((matched * 100.0) / total)
+            + "%");
+    // TO DO: Reader of this pipe line should read from xmi generated by
+    // SimpleRunCPE
+    double cAt1 = (((double) matched) / ((double) total) * unanswered + (double) matched)
+            * (1.0 / total);
 
-		for (int i = 0; i < cAt1ForAll.size(); i++) {
-			System.out.println(String.format("Performance for doc %d", i));
-			int matched = numCorrectForAll.get(i);
-			int total = numTotalAnswerForAll.get(i);
-			System.out.println("Correct: " + matched + "/" + total + "=" + ((matched * 100.0) / total) + "%");
-			System.out.println("c@1 score:" + cAt1ForAll.get(i));
-		}
+    cAt1ForAll.add(cAt1);
+    numCorrectForAll.add(matched);
+    numTotalAnswerForAll.add(total);
 
-		System.out.println("===============================================================");
+    System.out.println("c@1 score:" + cAt1);
+  }
 
-		System.out.println("==============Copy the following to GitHub Wiki!)==============");
+  public String findBestChoice(HashMap<String, Double> hshAnswer) throws Exception {
+    Iterator<String> it = hshAnswer.keySet().iterator();
+    String bestAns = null;
+    double maxScore = 0;
+    System.out.println("Aggregated counts; ");
+    while (it.hasNext()) {
+      String key = it.next();
+      Double val = hshAnswer.get(key);
+      System.out.println(key + "\t" + key + "\t" + val);
+      if (val > maxScore) {
+        maxScore = val;
+        bestAns = key;
+      }
+    }
+    return bestAns;
+  }
 
-		for (int i = 0; i < cAt1ForAll.size(); i++) {
-			System.out.println(String.format("      |    %d  | %f", i, cAt1ForAll.get(i)));
-		}
-		System.out.println("  ----|-------|--------");
+  @Override
+  public void collectionProcessComplete() throws AnalysisEngineProcessException {
+    System.out.println("======================Performance in text======================");
 
-		System.out.println("===============================================================");
+    for (int i = 0; i < cAt1ForAll.size(); i++) {
+      System.out.println(String.format("Performance for doc %d", i));
+      int matched = numCorrectForAll.get(i);
+      int total = numTotalAnswerForAll.get(i);
+      System.out.println("Correct: " + matched + "/" + total + "=" + ((matched * 100.0) / total)
+              + "%");
+      System.out.println("c@1 score:" + cAt1ForAll.get(i));
+    }
 
-	}
+    System.out.println("===============================================================");
+
+    System.out.println("==============Copy the following to GitHub Wiki!)==============");
+
+    for (int i = 0; i < cAt1ForAll.size(); i++) {
+      System.out.println(String.format("      |    %d  | %f", i, cAt1ForAll.get(i)));
+    }
+    System.out.println("  ----|-------|--------");
+
+    System.out.println("===============================================================");
+
+  }
 
 }
